0131b581640ad3d5796a6a6e5c3acd2105dc5233
Woojay Poynter <h7770nul@gmail.com>
2020-02-25 23:53:04 -0800

    Implements GET /fill/?symbol=xxx&bid=111&quantity=111

--- receiver.go
+++ receiver.go
@@ -35,13 +35,15 @@ type nbboData struct {
 	Time        string `json:"time,omitempty"`
 	BidPrice    string `json:"bidprice,omitempty"`
 	BidExchange string `json:"bidexchange,omitempty"`
+	BidSize     string `json:"bidsize,omitempty"`
 	AskPrice    string `json:"askprice,omitempty"`
 	AskExchange string `json:"askexchange,omitempty"`
+	AskSize     string `json:"asksize,omitempty"`
 }
 
 const timeFormat = "03:04:05"
 
-var nbboNow = nbboData{"", "00:00:00.001", "-1", "", "10000000", ""}
+var nbboNow = nbboData{"", "00:00:00.001", "-1", "", "", "10000000", "", ""}
 var nbboStore = map[string]nbboData{}
 
 var nbboProcessed = promauto.NewCounter(prometheus.CounterOpts{
@@ -135,6 +137,9 @@ func main() {
 				nbboNow.AskExchange = decoded.AskExchange
 			}
 
+			nbboNow.BidSize = decoded.BidSize
+			nbboNow.AskSize = decoded.AskSize
+
 			//// If new time, get set up for a new time group
 		} else if decodedTime.After(nbboNowTime) {
 
@@ -162,8 +167,10 @@ func main() {
 			nbboNow.Symbol = decoded.Symbol
 			nbboNow.BidPrice = decoded.BidPrice
 			nbboNow.BidExchange = decoded.BidExchange
+			nbboNow.BidSize = decoded.BidSize
 			nbboNow.AskPrice = decoded.AskPrice
 			nbboNow.AskExchange = decoded.AskExchange
+			nbboNow.AskPrice = decoded.AskPrice
 		}
 	})
 
@@ -171,27 +178,85 @@ func main() {
 
 		q := r.URL.Query()
 
-		reqSymbol, ok := q["symbol"]
-		if !ok || len(reqSymbol) == 0 {
+		reqSymbols, ok := q["symbol"]
+		if !ok || len(reqSymbols) == 0 {
 			fmt.Fprintf(w, "Missing symbol"+r.URL.RequestURI()) // Pseudo 400
 			log.Println("Missing symbol")
 		}
 
-		reqBid, ok := q["bid"]
-		if !ok || len(reqBid) == 0 {
+		reqBids, ok := q["bid"]
+		if !ok || len(reqBids) == 0 {
 			fmt.Fprintf(w, "Missing bid price"+r.URL.RequestURI())
 			log.Println("Missing bid")
 		}
 
-		reqQuantity, ok := q["quantity"]
-		if !ok || len(reqQuantity) == 0 {
+		reqQuantities, ok := q["quantity"]
+		if !ok || len(reqQuantities) == 0 {
 			fmt.Fprintf(w, "Missing quantity"+r.URL.RequestURI())
 			log.Println("Missing quantity")
 		}
 
-		w.WriteHeader(200)
-		w.Write([]byte("Good query received:" + r.URL.RawQuery + "<br>" + "Symbol: " + reqSymbol[0] + " Bid: " + reqBid[0] + " Qty: " + reqQuantity[0]))
-		log.Println("Good query received:" + r.URL.RawQuery + "<br>" + "Symbol: " + reqSymbol[0] + " Bid: " + reqBid[0] + " Qty: " + reqQuantity[0])
+		reqSymbol := reqSymbols[0]
+		reqBid, _ := strconv.ParseFloat(reqBids[0], 64)
+		reqQuantity, _ := strconv.ParseInt(reqQuantities[0], 10, 64)
+		fillOrders := []nbboData{}
+		filledQuantity := int64(0)
+
+		// Get Symbol
+		// keys := rdsClient.Keys("*")
+		listLen := rdsClient.LLen(reqSymbol).Val() // thread unsafe
+		if listLen == 0 {
+			w.WriteHeader(200)
+			w.Write([]byte("Symbol not found"))
+		} else {
+			for {
+				// Get Price
+				//// Get last NBBO
+				lastNbboRaw := rdsClient.LRange(reqSymbol, listLen-1, listLen-1)
+
+				// TODO:
+				// check we are not running out of NBBOs before moving on
+
+				temp := []byte(lastNbboRaw.Val()[0])
+				decodedMsg := nbboData{}
+				err := json.Unmarshal(temp, &decodedMsg)
+				if err != nil {
+					return
+				}
+
+				// If offer is less than or equal to willing bid, buy/fill
+				ask, _ := strconv.ParseFloat(decodedMsg.AskPrice, 64)
+				// bid, _ := strconv.ParseFloat(reqBid, 64)
+				if ask <= reqBid {
+					// Get Quantity -> will overfill before termination for now
+					askSize, _ := strconv.ParseInt(decodedMsg.AskSize, 10, 64)
+					filledQuantity += askSize
+
+					// Save the quote
+					fillOrders = append(fillOrders, decodedMsg)
+
+					// if quantity is matched or overfilled, done
+					if filledQuantity >= reqQuantity {
+						w.WriteHeader(200)
+						w.Write([]byte("Good query received:" + r.URL.RawQuery + "<br>" + "Symbol: " + reqSymbols[0] + " Bid: " + reqBids[0] + " Qty: " + reqQuantities[0] + "<P>"))
+
+						// Report each quotes in the fill
+						total := int64(0)
+						for _, fill := range fillOrders {
+							fillAsk, _ := strconv.ParseInt(fill.AskSize, 10, 32)
+							total += fillAsk
+							w.Write([]byte("@" + fill.Time + " Bid Price: " + fill.BidPrice + " Size: " + fill.BidSize + " Ask Price: " + fill.AskPrice + " Size: " + fill.AskSize + " Cumulative: " + string(total) + "<BR>"))
+							log.Println("@" + fill.Time + " Bid Price: " + fill.BidPrice + " Size: " + fill.BidSize + " Ask Price: " + fill.AskPrice + " Size: " + fill.AskSize + " Cumulative: " + strconv.Itoa(total))
+						}
+
+						log.Println("Good query received:" + r.URL.RawQuery + "<br>" + "Symbol: " + reqSymbols[0] + " Bid: " + reqBids[0] + " Qty: " + reqQuantities[0])
+
+						break
+					}
+				}
+			}
+		}
+
 	})
 
 	// Prometheus
